38e946da270899d224d475a2125c16c3ce393fd3
zoesacks <102333575+zoesacks@users.noreply.github.com>
2023-10-09 15:09:33 -0300

    Delete GESTION_PILAR directory

--- GESTION_PILAR/ingresos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from ingresos.views import aplicaciones_ingresos,calculadora,serivicios_generales
-
-urlpatterns = [
-    path('aplicaciones/', aplicaciones_ingresos, name='aplicaciones_ingresos'),
-    path('calculadora/', calculadora, name='calculadora'),
-    path('serivicios_generales/', serivicios_generales, name='serivicios_generales'),
-
-]
